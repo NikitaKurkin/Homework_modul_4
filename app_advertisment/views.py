ae5b34d2a598548309a38e7c583805ccc63cdb54
Nikita <nikitakurkin1961@yandex.ru>
2023-07-13 22:24:08 +0300

    Переходное сохранение

--- app_advertisment/views.py
+++ app_advertisment/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Это мой первый сайт')
+    return render(request, 'index.html')
