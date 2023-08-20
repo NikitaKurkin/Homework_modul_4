29962fa6ff9c19391b431a9576149ab629848272
Nikita <nikitakurkin1961@yandex.ru>
2023-08-20 12:11:28 +0300

    ДЗ за 9 урок

--- app_advertisment/urls.py
+++ app_advertisment/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from .views import index, top_sellers, advertisement_post, register, login, profile
+from .views import index, top_sellers
 
 urlpatterns = [
     path('', index, name='main_page'),
     path('top-sellers/', top_sellers, name='top_sellers'),
-    path('advertisement-post/', advertisement_post, name='advertisement_post'), 
-    path('register/', register, name='register'),
-    path('login/', login, name='login'),
-    path('profile/', profile, name='profile')
 ]
